9a3189ee9c45492c1c16cb1a2e2fc5a0f52cb7cb
Juan Carlos Reyes Meza <jcreyes@Juans-MacBook-Pro.local>
2023-10-27 13:34:39 -0500

    react-todo-list

---
+++ src/organisms/task-group.jsx
@@ -0,0 +1,6 @@
+import Task from '../molecules/task';
+import '../styles/styles.css';
+function TaskGroup({ taskList, onDeleteTask }) {
+  return taskList.map((task) => <Task key={task} taskText={task} onDeleteTask={onDeleteTask}/>);
+}
+export default TaskGroup;
